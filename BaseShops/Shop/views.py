e48f439ee3430d1807733c215d7bdcd7c2e2f87e
MaxViky <MaxViky@users.noreply.github.com>
2020-06-02 20:35:07 +0400

    Forms for POST

--- BaseShops/Shop/views.py
+++ BaseShops/Shop/views.py
@@ -1,7 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from Shop.models import *
+from Shop.forms import *
 
 
 def ViewShops(request):
     shops = Shop.objects.all()
     return render(request, 'MainApp/shops.html', {"shops": shops})
+
+
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        return HttpResponse("<h2>Hello, {0}</h2>".format(name))
+    else:
+        shopform = ShopsForm()
+        return render(request, "shops.html", {"ShopForm": shopform})
--- BaseShops/city/views.py
+++ BaseShops/city/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from city.models import *
 
 
+
 def CityView(request):
     city = City.objects.all()
     return render(request, "MainApp/cities.html", {"city": city})
+
 
 
