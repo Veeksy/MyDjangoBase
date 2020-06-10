f8a37cba823be4944fa10e2414fae035bcb32c43
MaxViky <MaxViky@users.noreply.github.com>
2020-06-10 16:29:27 +0400

    Revert "Finally"
    This reverts commit 6356648d

--- BaseShops/Shop/forms.py
+++ BaseShops/Shop/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ShopsForm(forms.Form):
     city = forms.CharField(label='Город', required=False)
     street = forms.CharField(label='Улица', required=False)
-    status = forms.IntegerField(label='Статус', required=False)
+    status = forms.CharField(label='Статус', required=False)
--- BaseShops/Shop/views.py
+++ BaseShops/Shop/views.py
@@ -4,9 +4,11 @@ from Shop.models import *
 from Shop.forms import ShopsForm
 from streets.models import *
 import json
+import datetime
 
 
 def ViewShops(request):
+    global Status
     shops = Shop.objects.all()
     shopform = ShopsForm(request.GET)
     count = Shop.objects.all().count()
@@ -17,16 +19,23 @@ def ViewShops(request):
             shops = Shop()
             shops.id = count+1
             shops.name = d['name']
-            shops.Streets = d['street']
+            street = Streets.objects.get(id=d['street'])
+            shops.street = street
             shops.house = d['house']
             shops.time_to_open = d['time_to_open']
             shops.time_to_close = d['time_to_close']
             Shop.save(shops)
-            return HttpResponse(shops.id)
+            return HttpResponse("<h2>Была создана запись {0}</h2>".format(shops.id))
+    TimeNow = datetime.datetime.now().time()
     if 'search' in request.GET:
         if shopform.is_valid():
             if shopform.cleaned_data["street"]:
-                shops = shops.filter(street=shopform.cleaned_data["street"])
-    return render(request, 'MainApp/shops.html', {"shops": shops, "ShopForm": shopform})
-
-
+                shops = shops.filter(street__name=shopform.cleaned_data["street"])
+            if shopform.cleaned_data["city"]:
+                shops = shops.filter(street__city__name=shopform.cleaned_data["city"])
+            if shopform.cleaned_data["status"]:
+                if shopform.cleaned_data["status"] == '1':
+                    shops = shops.filter(time_to_open__lt=TimeNow, time_to_close__gt=TimeNow)
+                if shopform.cleaned_data["status"] == '0':
+                    shops = shops.filter(time_to_open__lt=TimeNow, time_to_close__lt=TimeNow)
+    return render(request, 'MainApp/shops.html', {"shops": shops, "ShopForm": shopform, "servtime": TimeNow})
